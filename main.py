4c2d5b0702841794ca28da43959e5247cc78976f
jxia3 <97870693+jxia3@users.noreply.github.com>
2024-11-14 10:41:44 -0500

    add custom memory chain

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import jax
 import jax.numpy as jnp
 import gymnax
+from memory_chain import MemoryChain, EnvParams
 
-rng = jax.random.PRNGKey(0)
+rng = jax.random.PRNGKey(9)
 rng, key_reset, key_act, key_step = jax.random.split(rng, 4)
 
-env, env_params = gymnax.make("MemoryChain-bsuite")
+env, env_params = MemoryChain(num_bits=2), EnvParams(max_steps_in_episode=1000, memory_length=5)
 obs, state = env.reset(key_reset, env_params)
 done = False
 
 while not done:
-    action = 1
+    action = 0
     next_obs, next_state, reward, done, _ = env.step(key_step, state, action, env_params)
     print(obs, action, reward, next_obs)
     obs, state = next_obs, next_state
